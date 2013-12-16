57188498fe8e72338bb6a2316b7d0caf120e5530
DiVaz <snowblade90@gmail.com>
2013-12-16 18:19:07 +0000

    final countdown

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $.getJSON("./api/getInvoice.php?number=" + $("#form_invoice_number").val(), func
 	
 $("#invoice_list").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
 '<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
-'<div> <strong>CostumorID: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
 '<div>__________' + '</div>' );
 });
 }
@@ -45,9 +45,9 @@ function submit_invoice_form()
 {
 $.getJSON("./api/getInvoice.php?number=" + $("#form_invoice_number").val(), function(data){
 	
-$("#invoice_list").append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
+$("#invoice_list").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
 '<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
-'<div> <strong>CostumorID: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
 '<div>__________' + '</div>' );
 });
 }
@@ -57,9 +57,9 @@ function submit_search_invoice_form()
 {
 $.getJSON("./api/searchInvoicesByField.php?field=" + $("#form_invoice_camp").val() + "&number=" + $("#form_invoice_number2").val(), function(data){
 	
-$("#invoice_list2").append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
+$("#invoice_list2").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
 '<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
-'<div> <strong>CostumorID: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
 '<div>__________' + '</div>' );
 });
 }
@@ -68,7 +68,7 @@ $("#invoice_list2").append('<div class="invoice"><div> <strong>Codigo: </strong>
 function submit_search_customer_form()
 {
 $.getJSON("./api/searchCustomersByField.php?field=" + $("#form_customer_camp").val() + "&number=" + $("#form_customer_number2").val(), function(data){
-$("#customer_list2").append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
+$("#customer_list2").empty().append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
 '<div> <strong>Taxa: </strong>' + data[0].CustomerTaxID + '</div>' +
 '<div> <strong>Companhia: </strong>' + data[0].CompanyName + '</div>' +
 '<div> <strong>E-mail: </strong>' + data[0].Email + '</div>' +
@@ -82,7 +82,7 @@ function submit_search_customer_form2()
 {
 $.getJSON("./api/searchCustomersByField.php?field=" + $("#form_customer_camp2").val() + "&company=" + $("#form_customer_x").val(), function(data){
 	
-$("#customer_list3").append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
+$("#customer_list3").empty().append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
 '<div> <strong>Taxa: </strong>' + data[0].CustomerTaxID + '</div>' +
 '<div> <strong>Companhia: </strong>' + data[0].CompanyName + '</div>' +
 '<div> <strong>E-mail: </strong>' + data[0].Email + '</div>' +
@@ -96,7 +96,7 @@ function submit_search_product_form()
 {
 $.getJSON("./api/searchProductsByField.php?field=" + $("#form_product_camp").val() + "&number=" + $("#form_product_number2").val(), function(data){
 	
-$("#product_list2").append('<div class="produto"><div> <strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
+$("#product_list2").empty().append('<div class="produto"><div> <strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
 '<div> <strong>Descrição: </strong>' + data[0].ProductDescription + '</div>' +
 '<div> <strong>Unidade de medida: </strong>' + data[0].UnitOfMeasure + '</div>' +
 '<div> <strong>Preço: </strong>' + data[0].UnitPrice + '</div>'+
