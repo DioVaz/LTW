41614e26c69ac392484d636a485a399b768d4c09
fosb <fosbtank@gmail.com>
2013-12-15 22:28:14 -0800

    fixed

--- api/searchCustomersByField.php
+++ api/searchCustomersByField.php
@@ -2,30 +2,31 @@
 
 $base = new PDO('sqlite:base2.db');
 
-function DisplayCostumer($article){
-
-	if(is_array($article))
-		$x= array('CustomerID'=>$article['CustomerID'],'<br>'.'CustomerTaxID'=>$article['CustomerTaxID'],'<br>'.'CompanyName'=>$article['CompanyName'],'<br>'.'BillingAddressID'=>$article['BillingAddressID'],'<br>'.'Email'=>$article['Email']);
-			echo json_encode($x);
-			echo '<br />','<br />';
-}
 	
 if($_GET['field'] == 'CustomerID'){
 
-	$stmt = $base->prepare('SELECT * FROM customer WHERE CustomerID = :n');
-	$stmt->bindParam(':n',$_GET['number'], PDO::PARAM_INT);
+	$stmt = $base->prepare('SELECT * FROM customer WHERE CustomerID = :value1');
+	$stmt->bindParam(':value1',$_GET['number'], PDO::PARAM_INT);
 	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
 elseif($_GET['field'] == 'CompanyName'){
 
-	$stmt = $base->prepare('SELECT * FROM customer WHERE CompanyName = :n');
-	$stmt->bindParam(':n',$_GET['asd'], PDO::PARAM_STR);
+	$stmt = $base->prepare('SELECT * FROM customer WHERE CompanyName = :value2');
+	$stmt->bindParam(':value2',$_GET['company'], PDO::PARAM_STR);
 	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}	
 	
- 	while ($article=$stmt->fetch()){
-	DisplayCostumer($article);
-}	
+
+if(is_array($result)){
+		echo json_encode($result);
+	}
+	else{
+		$erro = array('code' => '404', 'reason' => 'Customer not found');
+		echo json_encode($erro);
+	}
+
 	
 ?>
--- script.js
+++ script.js
@@ -1,77 +1,75 @@
-function submit_login_form(){
-
-	console.log('cenas');
-	$.get("LogIn.php", {email: "ei09001@fe.up.pt", pass: "1234"});
-}
-	
 
 function submit_product_form()
 {
 $.getJSON("./api/getProduct.php?number=" + $("#form_product_number").val(), function(data){
 	
-$("#product_list").append('<div class="produto"><div><strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
+$("#product_list").empty().append('<div class="produto"><div><strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
 '<div><strong>Descrição: </strong>' + data[0].ProductDescription + '</div>' +
-'<div><strong>Unidade de medida:' + data[0].UnitOfMeasure + '</div>' +
+'<div><strong>Unidade de medida:</strong>' + data[0].UnitOfMeasure + '</div>' +
 '<div><strong>Preço: </strong>' + data[0].UnitPrice + '</div>'+
 '<div>__________' + '</div>' );
 });
 }
 
+
 function submit_customer_form()
 {
-
-
 $.getJSON("./api/getCustomer.php?number=" + $("#form_customer_number").val(), function(data){
 	
 $("#customer_list").empty().append('<div class="cliente"><div><strong>Numero de identificação: </strong>' + data[0].CustomerID + '</div>' +
 '<div><strong>Taxa: </strong>' + data[0].CustomerTaxID + '</div>' +
 '<div><strong>Empresa: </strong>' + data[0].CompanyName + '</div>' +
 '<div><strong>E-mail: </strong>' + data[0].Email + '</div></div>');
-
-
 });
 
 $.getJSON("./api/getAdress.php?number=" + $("#form_customer_number").val(), function(data){
 	
 $("#customer_list").append('<div class="adress"><div><strong>Morada:</strong>' + data[0].AdressDetail + '</div>'+
 '<div>__________' + '</div></div>' );
-
-
 });
-
 }
-
-
-
 
 
 function submit_invoice_form()
 {
 $.getJSON("./api/getInvoice.php?number=" + $("#form_invoice_number").val(), function(data){
 	
-$("#invoice_list").append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
+$("#invoice_list").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
 '<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
-'<div> <strong>CostumorID: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
 '<div>__________' + '</div>' );
 });
 }
+
+
+function submit_invoice_form()
+{
+$.getJSON("./api/getInvoice.php?number=" + $("#form_invoice_number").val(), function(data){
+	
+$("#invoice_list").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
+'<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
+'<div>__________' + '</div>' );
+});
+}
+
 
 function submit_search_invoice_form()
 {
-$.getJSON("./api/searchInvoiceByField.php?text=" + $("#form_invoice_camp").val() + "&?number=" + $("#form_invoice_number").val(), function(data){
+$.getJSON("./api/searchInvoicesByField.php?field=" + $("#form_invoice_camp").val() + "&number=" + $("#form_invoice_number2").val(), function(data){
 	
-$("#invoice_list2").append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
+$("#invoice_list2").empty().append('<div class="invoice"><div> <strong>Codigo: </strong>' + data[0].InvoiceNo + '</div>' +
 '<div> <strong>Data: </strong>' + data[0].InvoiceDate + '</div>' +
-'<div> <strong>CostumorID: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>CustomerID: </strong>' + data[0].CustomerID + '</div>' +
 '<div>__________' + '</div>' );
 });
 }
 
-function submit_search_costumer_form()
+
+function submit_search_customer_form()
 {
-$.getJSON("./api/searchCostumerByField.php?text=" + $("#form_costumer_camp").val() + "&?number=" + $("#form_costumer_number").val(), function(data){
-	
-$("#customer_list2").append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
+$.getJSON("./api/searchCustomersByField.php?field=" + $("#form_customer_camp").val() + "&number=" + $("#form_customer_number2").val(), function(data){
+$("#customer_list2").empty().append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
 '<div> <strong>Taxa: </strong>' + data[0].CustomerTaxID + '</div>' +
 '<div> <strong>Companhia: </strong>' + data[0].CompanyName + '</div>' +
 '<div> <strong>E-mail: </strong>' + data[0].Email + '</div>' +
@@ -80,17 +78,34 @@ $("#customer_list2").append('<div class="cliente"><div> <strong>Codigo: </strong
 });
 }
 
+
+function submit_search_customer_form2()
+{
+$.getJSON("./api/searchCustomersByField.php?field=" + $("#form_customer_camp2").val() + "&company=" + $("#form_customer_x").val(), function(data){
+	
+$("#customer_list3").empty().append('<div class="cliente"><div> <strong>Codigo: </strong>' + data[0].CustomerID + '</div>' +
+'<div> <strong>Taxa: </strong>' + data[0].CustomerTaxID + '</div>' +
+'<div> <strong>Companhia: </strong>' + data[0].CompanyName + '</div>' +
+'<div> <strong>E-mail: </strong>' + data[0].Email + '</div>' +
+'<div> <strong>BillingAddressID : </strong>' + data[0].BillingAddressID  + '</div>'+
+'<div>__________' + '</div>' );
+});
+}
+
+
 function submit_search_product_form()
 {
-$.getJSON("./api/searchProductByField.php?text=" + $("#form_product_camp").val() + "&?number=" + $("#form_product_number").val(), function(data){
+$.getJSON("./api/searchProductsByField.php?field=" + $("#form_product_camp").val() + "&number=" + $("#form_product_number2").val(), function(data){
 	
-$("#product_list2").append('<div class="produto"><div> <strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
+$("#product_list2").empty().append('<div class="produto"><div> <strong>Codigo: </strong>' + data[0].ProductCode + '</div>' +
 '<div> <strong>Descrição: </strong>' + data[0].ProductDescription + '</div>' +
 '<div> <strong>Unidade de medida: </strong>' + data[0].UnitOfMeasure + '</div>' +
 '<div> <strong>Preço: </strong>' + data[0].UnitPrice + '</div>'+
 '<div>__________' + '</div>' );
 });
 }
+
+
 
 $(document).ready(function(){
 $("#submit_button").click(submit_product_form);
@@ -110,14 +125,14 @@ $("#submit_button4").click(submit_search_invoice_form);
 })
 
 $(document).ready(function(){
-$("#submit_button5").click(submit_search_costumer_form);
+$("#submit_button5").click(submit_search_customer_form);
 })
 
 $(document).ready(function(){
 $("#submit_button6").click(submit_search_product_form);
-});
+
+})
 
 $(document).ready(function(){
-	console.log('teste 1');
-$("#form_customer_number").on('click',submit_login_form);
+$("#submit_button7").click(submit_search_customer_form2);
 });
